0b668fb432386dad88ca37183856beede3170ceb
Adam Cherek <111668308+adbreeker@users.noreply.github.com>
2025-09-28 14:46:44 +0200

    visual part of choose map panel

---
+++ Packages/com.adbreeker.tdmapcreator/Runtime/MapCreatorScene/UI/ChooseMapPanel.cs
@@ -0,0 +1,12 @@
+#if UNITY_EDITOR
+using UnityEngine;
+using adbreeker.TDMapCreator;
+
+namespace adbreeker.TDMapCreator
+{
+    public class ChooseMapPanel : MonoBehaviour
+    {
+       
+    }													
+}
+#endif
