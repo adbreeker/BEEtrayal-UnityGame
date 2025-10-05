b50e7af5319a106839d06b0ea3b8796777eeed05
Adam Cherek <111668308+adbreeker@users.noreply.github.com>
2025-10-05 23:18:07 +0200

    overriding editor shortcuts
    - in playmode: creating temp profile to override editor shortcuts
    - in editor: restoring shortcuts by removing temp profile

--- Packages/com.adbreeker.tdmapcreator/Editor/TDMapCreator_Setup.cs
+++ Packages/com.adbreeker.tdmapcreator/Editor/TDMapCreator_Setup.cs
@@ -1,10 +1,12 @@
 #if UNITY_EDITOR
 using adbreeker.TDMapCreator;
+using Codice.Utils;
 using System.IO;
 using System.Linq;
 using UnityEditor;
 using UnityEditor.PackageManager;
 using UnityEditor.SceneManagement;
+using UnityEditor.ShortcutManagement;
 using UnityEngine;
 
 
@@ -19,6 +21,9 @@ namespace adbreeker.TDMapCreator
             EditorApplication.playModeStateChanged -= PlaymodeChange;
             EditorApplication.playModeStateChanged += PlaymodeChange;
 
+            EditorApplication.quitting -= OnEditorQuit;
+            EditorApplication.quitting += OnEditorQuit;
+
             if (SessionState.GetBool(PackageVariables.EnvKeys.SESSIONSTATE_INITIALIZED, false)) { return; }
             SessionState.SetBool(PackageVariables.EnvKeys.SESSIONSTATE_INITIALIZED, true);
 
@@ -30,9 +35,9 @@ namespace adbreeker.TDMapCreator
         {
             PackageUtilis.PrintDebug(LogType.Log, $"Playmode state changed: {state}");
 
-            if(state == PlayModeStateChange.ExitingEditMode)
+            if (state == PlayModeStateChange.ExitingEditMode)
             {
-
+                OverrideShortcuts();
             }
             else if (state == PlayModeStateChange.EnteredPlayMode)
             {
@@ -41,10 +46,39 @@ namespace adbreeker.TDMapCreator
             else if (state == PlayModeStateChange.ExitingPlayMode)
             {
                 EditorSceneManager.playModeStartScene = null;
+                RestoreShortcuts();
             }
             else if (state == PlayModeStateChange.EnteredEditMode)
             {
 
+            }
+        }
+
+        static void OnEditorQuit()
+        {
+            RestoreShortcuts();
+        }
+
+        // Override Unity Shortcuts
+        private static void OverrideShortcuts()
+        {
+            var manager = ShortcutManager.instance;
+            string tempProfileId = "TDMapCreator-TempProfile";
+            manager.CreateProfile(tempProfileId);
+            manager.activeProfileId = tempProfileId;
+
+            // Disable the shortcut
+            manager.RebindShortcut("Main Menu/File/Save", ShortcutBinding.empty);
+        }
+
+        private static void RestoreShortcuts()
+        {
+            var manager = ShortcutManager.instance;
+            string tempProfileId = "TDMapCreator-TempProfile";
+
+            if (manager.GetAvailableProfileIds().Contains(tempProfileId))
+            {
+                manager.DeleteProfile(tempProfileId);
             }
         }
     }
